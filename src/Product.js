b64d33c3c71d7edd7d8668280a6aee8e2db5a6a9
mandar-purandare <mnpsstack@gmail.com>
2023-09-21 23:02:26 +0530

    use props for multiple products

--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Product.css'
 
-function Product() {
+function Product({id,title,price,rating,image}) {
   return (
     <div>
         <div className="product">
             <div className="product__info">
-                <p>Title</p>
+                <p>{title}</p>
                 <p className="product__price">
                   <small>$</small>
-                  <strong>30</strong>
+                  <strong>{price}</strong>
                 </p>
-                <div className="product__rating">⭐⭐</div>
+                <div className="product__rating">
+                  {Array(rating)
+                   .fill()
+                   .map((_,i) => (
+                      <p>⭐</p>
+                   )) 
+                  }
+                </div>
             </div>
-            <img src='' alt='product-image'/>
+            <img src={image} alt='product-image'/>
             <button>Add to Basket</button>
         </div>
     </div>
